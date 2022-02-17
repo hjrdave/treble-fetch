93c1e82cdaf8591cd8fac7b03a594948fe9a8094
hjrdave <dsanders_m633@outlook.com>
2022-02-17 00:06:13 -0500

    updated useFetch

--- src/use-fetch/use-fetch.ts
+++ src/use-fetch/use-fetch.ts
@@ -5,32 +5,50 @@
 import React from "react";
 import { useNonInitialMountEffect } from '../hooks';
 
-interface IOptions extends RequestInit {
+export interface IOptions {
+    method?: 'GET' | 'POST' | 'DELETE' | 'PUT' | 'PATCH' | string;
+    headers?: HeadersInit;
+    body?: { [key: string]: any };
     timeout?: number | boolean;
     onTimeout?: () => void;
+    cache?: RequestCache;
+    credentials?: RequestCredentials;
+    integrity?: string;
+    keepalive?: boolean;
+    mode?: RequestMode;
+    redirect?: RequestRedirect;
+    referrer?: string;
+    referrerPolicy?: ReferrerPolicy;
+    window?: null;
 }
-const useFetch = (url: string, options?: IOptions) => {
+const useFetch = (url: RequestInfo, options?: IOptions) => {
 
-    const { method: _method, body, timeout, onTimeout, ..._options } = {
-        method: 'Get',
-        body: {},
-        timeout: (options?.timeout !== undefined) ? options.timeout : 20000,
-        onTimeout: () => null,
-        ...options
-    };
-
+    const [method, setMethod] = React.useState((options?.method) ? options.method : 'GET');
+    const [body, setBody] = React.useState(options?.body);
+    const [headers, setHeaders] = React.useState(options?.headers);
+    const [fetchTimeout] = React.useState((options?.timeout !== undefined) ? options.timeout : 20000);
     const [triggerResetState, setTriggerResetState] = React.useState([]);
     const [triggerFetch, setTriggerFetch] = React.useState([]);
-    const [mainURL, setMainUrl] = React.useState(url);
-    const [method, setMethod] = React.useState(_method);
+    const [mainURL, setMainURL] = React.useState(url);
+    const [routeURL, setRouteURL] = React.useState('');
     const [response, setResponse] = React.useState<Response | { data: any[] }>({ data: [] });
     const [error, setError] = React.useState<object | string | null>(null);
     const [loading, setLoading] = React.useState(false);
     const [abortController, setAbortController] = React.useState<AbortController>(new AbortController());
-    const get = () => setTriggerFetch([]);
-    // const post = () => {
-    //     setMethod('POST');
-    // };
+    const onTimeout = () => (options?.onTimeout) ? options.onTimeout() : null;
+
+    const get = (route?: string) => {
+        setMethod('GET')
+        setRouteURL((route) ? route : '');
+        setTriggerFetch([]);
+    };
+    const post = (body: { [key: string]: any }, route?: string) => {
+        setMethod('POST');
+        setRouteURL((route) ? route : '');
+        setBody(body);
+        setTriggerFetch([]);
+    };
+
     const abort = () => abortController.abort();
     const reset = () => setTriggerResetState([]);
 
@@ -38,11 +56,12 @@ const useFetch = (url: string, options?: IOptions) => {
 
         try {
             setLoading(true);
-            const jsFetch = fetch(url, {
+            const jsFetch = fetch(`${mainURL}${routeURL}`, {
                 ...options,
                 method: method,
                 signal: signal,
-                body: JSON.stringify(options?.body)
+                body: JSON.stringify(body),
+                headers: headers
             });
             let res = await jsFetch;
             const json = await res.json();
@@ -71,22 +90,28 @@ const useFetch = (url: string, options?: IOptions) => {
         const abortInstance = new AbortController();
         setAbortController(abortInstance);
         getFetch(abortInstance.signal);
-        if (typeof timeout === 'number') {
+        if (typeof fetchTimeout === 'number') {
             setTimeout(() => {
                 abortInstance.abort();
                 onTimeout();
-            }, timeout);
+            }, fetchTimeout);
         }
         return function cleanup() {
             abortInstance.abort();
         };
     }, [triggerFetch]);
 
+    useNonInitialMountEffect(() => {
+        setMainURL(mainURL);
+        setHeaders(headers)
+    }, [mainURL, headers]);
+
     return {
         response,
         error,
         loading,
         get,
+        post,
         abort,
         reset
     };
